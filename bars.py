55c2ff03f6619e74f1c9ab2e6dd96ef5eccd4547
BlackSan11 <bhekpdig@gmail.com>
2017-10-17 15:21:45 +0300

    f

--- bars.py
+++ bars.py
@@ -2,36 +2,50 @@
 import math
 import sys
 
-#выгружаем JSON из файла
-def load_data_from_file(filepath):
-    with open(filepath, "r", encoding='utf-8') as file_with_json:
-        json_from_file = file_with_json.read()
-        loaded_json = json.loads(json_from_file)
-        return loaded_json
 
-#ищем самый вместительный бар
+def load_data_from_file(filepath):
+    with open(filepath, "r", encoding="utf-8") as file_with_json:
+        json_from_file = json.load(file_with_json)
+        return json_from_file
+
+
 def get_biggest_bar(json_data):
     all_bars = json_data['features']
     bar = max(all_bars, key=lambda x: x['properties']['Attributes']['SeatsCount'])
-    return bar
+    return bar['properties']['Attributes']
+
 
 def get_smallest_bar(json_data):
     all_bars = json_data['features']
     bar = min(all_bars, key=lambda x: x['properties']['Attributes']['SeatsCount'])
-    return bar
+    return bar['properties']['Attributes']
+
 
 def get_closest_bar(json_data, me_X_coordinate, me_Y_coordinate):
     all_bars = json_data['features']
     bar = min(all_bars, key=lambda x: math.sqrt(((float(me_X_coordinate) - x['geometry']['coordinates'][0])**2) + ((float(me_Y_coordinate) - x['geometry']['coordinates'][1])**2)))
-    return bar['properties']['Attributes']['Name']
+    return bar['properties']['Attributes']
+
 
 if __name__ == '__main__':
-    datat = get_biggest_bar(load_data_from_file(sys.argv[1]))
-    datatmin = get_smallest_bar(load_data_from_file(sys.argv[1]))
-    print("Наиболей вместимостью славится \"{}\" - {} мест, находящийся по адресу: г. Москва, {}, телефон - +7 {}".format(datat['properties']['Attributes']['Name'],datat['properties']['Attributes']['SeatsCount'],datat['properties']['Attributes']['Address'],datat['properties']['Attributes']['PublicPhone'][0]['PublicPhone']))
+    biggest_bar = get_biggest_bar(load_data_from_file(sys.argv[1]))
+    smallest_bar = get_smallest_bar(load_data_from_file(sys.argv[1]))
+    closest_bar = get_closest_bar(load_data_from_file(sys.argv[1]), sys.argv[3], sys.argv[2])
     print(
-        "Самой маленькой вместимостью славится \"{}\" - {} мест, находящийся по адресу: г. Москва, {}, телефон - +7 {}".format(
-            datatmin['properties']['Attributes']['Name'], datatmin['properties']['Attributes']['SeatsCount'],
-            datatmin['properties']['Attributes']['Address'],
-            datatmin['properties']['Attributes']['PublicPhone'][0]['PublicPhone']))
-    print("Рядом с Вами {}".format(get_closest_bar(load_data_from_file(sys.argv[1]), sys.argv[2], sys.argv[3])))
+        "Хотите много места? Тогда вам в \"{}\" в нем {} мест, он находится по адресу: г. Москва, {}, телефон - +7 {}".format(
+            biggest_bar['Name'],
+            biggest_bar['SeatsCount'],
+            biggest_bar['Address'],
+            biggest_bar['PublicPhone'][0]['PublicPhone']))
+    print(
+        "Самым маленьким заведением является \"{}\" - {} мест, находящийся по адресу: г. Москва, {}, телефон - +7 {}".format(
+            smallest_bar['Name'],
+            smallest_bar['SeatsCount'],
+            smallest_bar['Address'],
+            smallest_bar['PublicPhone'][0]['PublicPhone']))
+    print(
+        "Рядом с Вами находиться {}, в нем - {} мест, он находиться по адресу: г. Москва, {}, телефон - +7 {}".format(
+            closest_bar['Name'],
+            closest_bar['SeatsCount'],
+            closest_bar['Address'],
+            closest_bar['PublicPhone'][0]['PublicPhone']))
--- test.py
+++
@@ -1,7 +0,0 @@
-foo = [2, 18, 9, 22, 17, 24, 8, 12, 27]
-
-print (filter(lambda x: x % 3 == 0, foo))
-
-print (map(lambda x: x * 2 + 10, foo))
-
-
