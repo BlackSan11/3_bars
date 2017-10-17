53253498f0d460b5eebd7f8bd2af896e9dc2b828
bogdan <bhelpdig@gmail.com>
2017-10-18 01:28:19 +0300

    uptodate

--- bars.py
+++ bars.py
@@ -1,43 +1,41 @@
 import json
 import math
-import sys
+import argparse
 
+parser = argparse.ArgumentParser(description='Great Description To Be Here')
 
 def load_data_from_file(filepath):
     with open(filepath, "r", encoding="utf-8") as file_with_json:
         json_from_file = json.load(file_with_json)
-        return json_from_file
+        return json_from_file['features']
 
 
 def get_biggest_bar(json_data):
-    all_bars = json_data['features']
-    bar = max(all_bars,
+    bar = max(json_data,
               key=lambda x: x['properties']['Attributes']['SeatsCount'])
     return bar['properties']['Attributes']
 
 
 def get_smallest_bar(json_data):
-    all_bars = json_data['features']
-    bar = min(all_bars,
+    bar = min(json_data,
               key=lambda x: x['properties']['Attributes']['SeatsCount'])
     return bar['properties']['Attributes']
 
 
-def get_closest_bar(json_data, me_x_coordinate, me_y_coordinate):
-    all_bars = json_data['features']
-    bar = min(all_bars,
-              key=lambda x: math.sqrt(((float(me_x_coordinate) -
+def get_closest_bar(json_data, me_longitude, me_latitude):
+    bar = min(json_data,
+              key=lambda x: math.sqrt(((float(me_longitude) -
                                         x['geometry']['coordinates'][0])**2) +
-                                      ((float(me_y_coordinate) -
+                                      ((float(me_latitude) -
                                         x['geometry']['coordinates'][1])**2)))
     return bar['properties']['Attributes']
 
 
 if __name__ == '__main__':
-    biggest_bar = get_biggest_bar(load_data_from_file(sys.argv[1]))
-    smallest_bar = get_smallest_bar(load_data_from_file(sys.argv[1]))
-    closest_bar = get_closest_bar(load_data_from_file(sys.argv[1]),
-                                  sys.argv[3], sys.argv[2])
+    json_from_file = load_data_from_file(sys.argv[1])
+    biggest_bar = get_biggest_bar(json_from_file)
+    smallest_bar = get_smallest_bar(json_from_file)
+    closest_bar = get_closest_bar(json_from_file, sys.argv[3], sys.argv[2])
     print(
         "Хотите много места? Тогда вам в \"{}\" в нем {} мест, "
         "он находится по адресу: г. Москва, {}, телефон - +7 {}".format(
@@ -53,7 +51,7 @@ if __name__ == '__main__':
             smallest_bar['Address'],
             smallest_bar['PublicPhone'][0]['PublicPhone']))
     print(
-        "Рядом с Вами находиться {}, в нем - {} мест,"
+        "Рядом с Вами находится {}, в нем - {} мест,"
         " он находиться по адресу: г. Москва, {}, телефон - +7 {}".format(
             closest_bar['Name'],
             closest_bar['SeatsCount'],
